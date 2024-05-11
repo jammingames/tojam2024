27049eededb3a41d2bf11d4ad5b26d14935bf516
ben <benjamin.j.langerak@gmail.com>
2024-05-11 15:53:58 -0400

    camera stuff i think?

--- ToJam2024/Assets/Scripts/WheelGoZoom.cs
+++ ToJam2024/Assets/Scripts/WheelGoZoom.cs
@@ -3,14 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public class CarData : ScriptableObject
+{
+    public float force = 5.0f;
+    public bool shouldRun = false;
+    
+}
 public class WheelGoZoom : MonoBehaviour
 {
+    // private CarData data;
     public float torqueSpeed = 5.0f;
     public bool shouldRun = false;
     private Rigidbody rb;
     private void Awake()
     {
+        // if (data == null) data = createscrip
         rb = GetComponent<Rigidbody>();
         
     }
