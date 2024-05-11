f8628fb6ad22d34afc0049fb993311392b3bd551
ben <benjamin.j.langerak@gmail.com>
2024-05-11 07:44:47 -0400

    adding some helpful comments. TODO: make this an extensionmethod

--- ToJam2024/Assets/Scripts/AttachmentManager.cs
+++ ToJam2024/Assets/Scripts/AttachmentManager.cs
@@ -17,10 +17,11 @@ public class AttachmentManager : MonoBehaviour
     
     public void Connect(AttachableObject obj, AttachableObject obj2)
     {
+        //need to map out and understand this diff
         var diff = Quaternion.Inverse(obj.currentJoint.transform.rotation) * obj.transform.rotation;
-
+        //vector pointing towards the other attachment point
         var dir2 = Quaternion.LookRotation(obj2.currentJoint.transform.forward * -1);
-        obj.transform.rotation = dir2 * diff;
+        obj.transform.rotation = dir2 * diff; //multiplying is order dependent. B local is applied to A global.
         
         Vector3 newPos = obj2.currentJoint.transform.position - (obj.currentJoint.transform.position - obj.transform.position);
         
